3cdf243c3947c626b118d74cb515c082c37e99ab
quoctoanpk2511 <quoctoanpk2511@gmail.com>
2019-12-11 17:19:51 +0700

    add community page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,61 @@
 @extends('layouts.master')
 
 @section('content')
+<div class="slider_area ">
+    <div class="bradcam_area breadcam_bg overlay2">
+        <h3>Community</h3>
+    </div>
+</div>
 
+<div class="container">
+         <div class="row">
+            <div class="col-lg-8 posts-list">
+               <div class="comments-area">
+                  <div class="comment-list">
+                     <div class="single-comment justify-content-between d-flex">
+                        <div class="user justify-content-between d-flex">
+                           <div class="thumb">
+                              <img src="img/comment/comment_1.png" alt="">
+                           </div>
+                           <div class="desc">
+                              <p class="comment">
+                                 Multiply sea night grass fourth day sea lesser rule open subdue female fill which them
+                                 Blessed, give fill lesser bearing multiply sea night grass fourth day sea lesser
+                              </p>
+                              <div class="d-flex justify-content-between">
+                                 <div class="d-flex align-items-center">
+                                    <h5>
+                                       <a href="#">Emilly Blunt</a>
+                                    </h5>
+                                    <p class="date">December 4, 2017 at 3:12 pm </p>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="col-lg-4">
+               <div class="blog_right_sidebar">
+               <div class="comment-form">
+                        <h4>Comment</h4>
+                        <form class="form-contact comment_form" action="#" id="commentForm">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <textarea class="form-control w-100" name="comment" id="comment" cols="30" rows="9"
+                                            placeholder="Write Comment"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="button button-contactForm btn_1 boxed-btn">Send Message</button>
+                            </div>
+                        </form>
+                    </div>  
+               </div>
+            </div>
+         </div>
+      </div>
 @endsection
