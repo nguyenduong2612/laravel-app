0d4b5b9b7b7b0ab1014d3e0b6c9d6318e505cc87
nguyenduong2612 <nguyenduong261298@gmail.com>
2019-12-18 04:20:18 +0700

    update database heroku

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$DATABASE_URL=parse_url('postgres://kevimkbigfdjvv:41ebe9f393fedc2ccebf6370c36c94b2083b833329573f1ff37c3c8aabbef797@ec2-184-73-176-11.compute-1.amazonaws.com:5432/d91l20gmml3tmj');
+
 return [
 
     /*
@@ -13,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),  // mysql for local or pgsql for heroku
 
     /*
     |--------------------------------------------------------------------------
@@ -58,11 +60,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['path'],'/'),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
