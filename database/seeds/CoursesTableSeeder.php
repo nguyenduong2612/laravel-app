e35a44344d7d8cb2daf2058ef73fa0b5797fdf84
quanganh2310 <ailavip1st@gmail.com>
2019-12-17 15:56:19 +0700

    Update Welcome page

--- database/seeds/CoursesTableSeeder.php
+++ database/seeds/CoursesTableSeeder.php
@@ -13,6 +13,61 @@ class CoursesTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        Course::create([
+            'title' => 'Mobile App design step by step from beginner',
+            'description' => $faker->text($maxNbChars = 100),
+            'image' => $faker->image('public/storage/',400,300, 'abstract', false),
+            'video' => $faker->image('public/storage/',400,300, 'abstract', false),
+            'cost' => rand(20, 100),
+            'subject_id' => rand(1,7),
+            'teacher_id' => 1
+        ]);
+        Course::create([
+            'title' => 'UI/UX design with Adobe XD with Anderson',
+            'description' => $faker->text($maxNbChars = 100),
+            'image' => $faker->image('public/storage/',400,300, 'abstract', false),
+            'video' => $faker->image('public/storage/',400,300, 'abstract', false),
+            'cost' => rand(20, 100),
+            'subject_id' => rand(1,7),
+            'teacher_id' => 1
+        ]);
+        Course::create([
+            'title' => 'Wordpress theme development from scratch',
+            'description' => $faker->text($maxNbChars = 100),
+            'image' => $faker->image('public/storage/',400,300, 'abstract', false),
+            'video' => $faker->image('public/storage/',400,300, 'abstract', false),
+            'cost' => rand(20, 100),
+            'subject_id' => rand(1,7),
+            'teacher_id' => 1
+        ]);
+        Course::create([
+            'title' => 'Mobile App design step by step from beginner',
+            'description' => $faker->text($maxNbChars = 100),
+            'image' => $faker->image('public/storage/',400,300, 'abstract', false),
+            'video' => $faker->image('public/storage/',400,300, 'abstract', false),
+            'cost' => rand(20, 100),
+            'subject_id' => rand(1,7),
+            'teacher_id' => 1
+        ]);
+        Course::create([
+            'title' => 'How to complete user research and make work flow',
+            'description' => $faker->text($maxNbChars = 100),
+            'image' => $faker->image('public/storage/',400,300, 'abstract', false),
+            'video' => $faker->image('public/storage/',400,300, 'abstract', false),
+            'cost' => rand(20, 100),
+            'subject_id' => rand(1,7),
+            'teacher_id' => 1
+        ]);
+        Course::create([
+            'title' => 'Commitment to dedicated Support',
+            'description' => $faker->text($maxNbChars = 100),
+            'image' => $faker->image('public/storage/',400,300, 'abstract', false),
+            'video' => $faker->image('public/storage/',400,300, 'abstract', false),
+            'cost' => rand(20, 100),
+            'subject_id' => rand(1,7),
+            'teacher_id' => 1
+        ]);
+
         
         $limit = 10;
 
